87bd10bfc12d04b7a02707960291e55fae080a55
Terry Xu <knight3001@gmail.com>
2022-01-26 15:57:27 +1000

    authentication

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,6 +7,14 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+    t.field("postedBy", {
+      type: "User",
+      resolve(parent, args, context) {
+        return context.prisma.link
+          .findUnique({ where: { id: parent.id } })
+          .postedBy();
+      },
+    });
   },
 });
 
@@ -36,10 +44,17 @@ export const LinkMutation = extendType({
       },
 
       resolve(parent, args, context) {
+        const { userId } = context;
+
+        if (!userId) {
+          throw new Error("Cannot post without logging in.");
+        }
+
         const newLink = context.prisma.link.create({
           data: {
             description: args.description,
             url: args.url,
+            postedBy: { connect: { id: userId } },
           },
         });
 
